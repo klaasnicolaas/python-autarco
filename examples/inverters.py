89e699f910153e43afe48f1866e07dbf01c07d15
Klaas Schoute <klaas_schoute@hotmail.com>
2024-07-11 13:51:53 +0200

    Update inverter example

--- examples/inverters.py
+++ examples/inverters.py
@@ -13,7 +13,25 @@ async def main() -> None:
     ) as client:
         account_sites: list[AccountSite] = await client.get_account()
         inverters = await client.get_inverters(account_sites[0].public_key)
+        power = await client.get_power_statistics(
+            account_sites[0].public_key,
+            query_range="day",
+        )
+        energy = await client.get_energy_statistics(
+            account_sites[0].public_key,
+            query_range="month",
+        )
+
+        print("--- INVERTERS ---")
         print(inverters)
+        print()
+
+        print("--- POWER STATS ---")
+        print(power.generate_power_stats_inverter)
+        print()
+
+        print("--- ENERGY STATS ---")
+        print(energy.generate_energy_stats_inverter)
 
 
 if __name__ == "__main__":
