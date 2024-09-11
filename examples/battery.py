5dd9db9ee694ce1ef8fb0b993b02ba52a8464b23
Klaas Schoute <klaas_schoute@hotmail.com>
2024-09-11 22:44:02 +0200

    Add example code to get battery data

--- examples/battery.py
+++ examples/battery.py
@@ -2,18 +2,18 @@
 
 import asyncio
 
-from autarco import AccountSite, Autarco, Solar
+from autarco import AccountSite, Autarco, Battery
 
 
 async def main() -> None:
-    """Show example on getting Autarco - solar data."""
+    """Show example on getting Autarco - battery data."""
     async with Autarco(
         email="test@autarco.com",
         password="password",
     ) as client:
         account_sites: list[AccountSite] = await client.get_account()
-        solar: Solar = await client.get_solar(account_sites[0].public_key)
-        print(solar)
+        battery: Battery = await client.get_battery(account_sites[0].public_key)
+        print(battery)
 
 
 if __name__ == "__main__":
